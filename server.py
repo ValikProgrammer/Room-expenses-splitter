2380d4723d718e5f394daa01304b6a975fe1bbc7
Vector <ValikProgrammer@gmail.com>
2025-11-13 23:23:27 +0200

    CI/CD

--- server.py
+++ server.py
@@ -32,4 +32,4 @@ with app.app_context():
 if __name__ == "__main__":
     with app.app_context():
         initialize_database()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
